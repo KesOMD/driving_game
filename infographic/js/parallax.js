71322f0b6714bc54f0907f102f67cdd622e2db8e
KesOMD <kester.brahim@omd.com>
2013-10-01 11:11:25 +0100

    finished infographic

--- infographic/js/parallax.js
+++ infographic/js/parallax.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 	
-	//redrawDotNav();
+	changeCar();
 	
 	/* Scroll event handler */
     $(window).bind('scroll',function(e){
     	parallaxScroll();
-		//redrawDotNav();
+		changeCar();
     });
     
 	/* Next/prev and primary nav btn click handlers */
@@ -60,31 +60,62 @@ $(document).ready(function() {
 function parallaxScroll()
 {
 	var scrolled = $(window).scrollTop();
-	//$('#parallax-bg1').css('top',(0-(scrolled*.25))+'px');
-	//$('#parallax-bg2').css('top',(0-(scrolled*.5))+'px');
-	//$('#parallax-bg3').css('top',(0-(scrolled*.75))+'px');
 	$('#invader-container1').css( 'top', ( 0 - ( scrolled * .75 ) ) + 'px');
 	$('#invader-container2').css( 'top', ( 0 - ( scrolled * .5 ) ) + 'px');
 	$('#popup-container').css( 'top', ( 0 - ( scrolled * .6 ) ) + 'px');
+	$('#palm-container2').css( 'top', ( 0 - ( scrolled * .8 ) ) + 'px');
 }
 
-/* Set navigation dots to an active state as the user scrolls */
-function redrawDotNav()
+/* change the car image after every section */
+function changeCar()
 {
 	var section1Top =  0;
-	// The top of each section is offset by half the distance to the previous section.
-	var section2Top =  $('#frameless-parachute').offset().top - (($('#english-channel').offset().top - $('#frameless-parachute').offset().top) / 2);
-	var section3Top =  $('#english-channel').offset().top - (($('#about').offset().top - $('#english-channel').offset().top) / 2);
-	var section4Top =  $('#about').offset().top - (($(document).height() - $('#about').offset().top) / 2);;
-	$('nav#primary a').removeClass('active');
-	if($(document).scrollTop() >= section1Top && $(document).scrollTop() < section2Top){
-		$('nav#primary a.manned-flight').addClass('active');
-	} else if ($(document).scrollTop() >= section2Top && $(document).scrollTop() < section3Top){
-		$('nav#primary a.frameless-parachute').addClass('active');
-	} else if ($(document).scrollTop() >= section3Top && $(document).scrollTop() < section4Top){
-		$('nav#primary a.english-channel').addClass('active');
-	} else if ($(document).scrollTop() >= section4Top){
-		$('nav#primary a.about').addClass('active');
+	var section2Top = $('#d1').offset().top - 200;
+	var section3Top = $('#d2').offset().top - 130;
+	var section4Top = $('#d3').offset().top - 130;
+	var section5Top = $('#d4').offset().top - 130;
+	var section6Top = $('#d5').offset().top + 500;
+	var section7Top = $('#d6').offset().top - 130;
+
+	$('#main-car').removeClass('car1');
+	if($(document).scrollTop() >= section1Top && $(document).scrollTop() < section2Top)
+	{
+		$('#main-car').removeClass('car2');
+		$('#main-car').addClass('car1');
 	}
-	
+	else if ($(document).scrollTop() >= section2Top && $(document).scrollTop() < section3Top)
+	{
+		$('#main-car').removeClass('car1');
+		$('#main-car').removeClass('car3');
+		$('#main-car').addClass('car2');
+	}
+	else if ($(document).scrollTop() >= section3Top && $(document).scrollTop() < section4Top)
+	{
+		$('#main-car').removeClass('car2');
+		$('#main-car').removeClass('car4');
+		$('#main-car').addClass('car3');
+	}
+	else if ($(document).scrollTop() >= section4Top && $(document).scrollTop() < section5Top)
+	{
+		$('#main-car').removeClass('car3');
+		$('#main-car').removeClass('car5');
+		$('#main-car').addClass('car4');
+	}
+	else if ($(document).scrollTop() >= section5Top && $(document).scrollTop() < section6Top)
+	{
+		$('#main-car').removeClass('car4');
+		$('#main-car').removeClass('car6');
+		$('#main-car').addClass('car5');
+	}
+	else if ($(document).scrollTop() >= section6Top && $(document).scrollTop() < section7Top)
+	{
+		$('#main-car').removeClass('car5');
+		$('#main-car').removeClass('car7');
+		$('#main-car').addClass('car6');
+	}
+	else if ($(document).scrollTop() >= section7Top)
+	{
+		$('#main-car').removeClass('car6');
+		$('#main-car').addClass('car7');
+	}
 }
